a8348779dde68341e3d6e58a646bd88913f8295d
Sahiti Chebolu <csahiti07@gmail.com>
2024-02-07 14:54:49 +0100

    local changes

--- plotter.py
+++ plotter.py
@@ -41,3 +41,21 @@ def example_trajectories(trajectories, color, lwidth, number):
     for i in range(number):
         plt.plot(trajectories[i], color=color,
                  linewidth=lwidth, linestyle='dashed')
+
+
+# %%
+custom_lines = [Line2D([0], [0], color='indigo', lw=2),
+                Line2D([0], [0], color='tab:blue', lw=2),
+                Line2D([0], [0], color='orange', lw=2)]
+
+fig, ax = plt.subplots(figsize=(2, 2), dpi=300)
+ax.axis('off')
+ax.legend(custom_lines, ['0.98',
+                         '0.6',
+                         '0.3'], title=r'$\eta$',
+          frameon=False)
+
+plt.savefig(
+    'plots/vectors/basic_legend_efficacy.svg',
+    format='svg', dpi=300,  bbox_inches='tight'
+)

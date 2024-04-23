8d4a6c09865f11f5d78c2901eaf434209c028162
Sahiti Chebolu <schebolu@mpi.localnet>
2024-04-23 17:24:22 +0200

    functios to compute avg distances between two sets of trajectories: real and simulated, for each cluster

---
+++ compute_distance.py
@@ -0,0 +1,49 @@
+"""
+module of functions to find average distance between two sets of trajectories;
+used to find the average distance between simulated trajectories from a model
+and each of the clusters
+"""
+
+from scipy.spatial import distance_matrix
+import numpy as np
+import ast
+
+
+def literal_eval(data, column_name):
+
+    literal_array = []
+    for i in range(len(data)):
+
+        literal_array.append(
+            ast.literal_eval(data[column_name][i]))
+
+    return np.array(literal_array)
+
+
+def avg_distance_clusters(cluster1, cluster2):
+    """
+    distance metric is L2 norm (euclidean distance) by default
+    """
+
+    pairwise_distances = distance_matrix(cluster1, cluster2)
+    avg_distance = np.mean(pairwise_distances)
+
+    return avg_distance
+
+
+def avg_distance_all_clusters(data, labels, simulated_data):
+    """
+    data: trajectories from real data
+    labels: from data specifying clusters
+    simulated data: simulated trajectories from model
+    """
+
+    avg_distances = []
+    for cluster in np.unique(labels):
+
+        # select data from a cluster & find avg dist. to simulated trajectory
+        avg_distances.append(
+            avg_distance_clusters(data[np.where(labels == cluster)[0]],
+                                  simulated_data))
+
+    return avg_distances

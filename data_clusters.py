a1be8b1e0dc22466829c5d0fc76f03aba056432f
Sahiti Chebolu <csahiti07@gmail.com>
2024-07-19 19:42:23 +0200

    avoid loops with pandas, remove unwanted lines

--- data_clusters.py
+++ data_clusters.py
@@ -16,62 +16,74 @@ mpl.rcParams['lines.linewidth'] = 3
 SAVE_ONLY = True
 
 # %%
+
+
+def normalize_cumulative_progress(row):
+    """
+    normalise cumulative progress by total credits
+    """
+    temp = np.array(ast.literal_eval(row['cumulative progress']))
+    return temp / row['Total credits']
+
+
+def process_delta_progress(row, semester_length_weeks):
+    """
+    aggregate delta progress over days to weeks
+    """
+    temp = ast.literal_eval(row['delta progress'])
+    temp_week = [sum(temp[i_week*7: (i_week+1)*7])
+                 for i_week in range(semester_length_weeks)]
+
+    assert sum(temp_week) == row['Total credits']
+    return temp_week
+
+
+def cumulative_progress_weeks(row):
+    """
+    get cumulative progress in weeks from delta progress in weeks
+    """
+    return list(np.cumsum(row['delta_progress_weeks']))
+
+
+# %%
 # pre-processing and clustering
-# already clustered data is saved as csv, no need to run this agian
 
 data = pd.read_csv('FollowUpStudymatrixDf_finalpaper.csv')
 
-# drop nan entries
-data_relevant = data.dropna(subset=['delta progress'])
-data_relevant = data_relevant.reset_index(drop=True)
-
 # drop the ones that discontinued (subj. 1, 95, 111)
 # sbj 24, 55, 126 also dont finish 7 hours but not because they drop out
-data_relevant = data_relevant.drop([1, 90, 104])
+data_relevant = data.drop([1, 95, 111])
 data_relevant = data_relevant.reset_index(drop=True)
 
-# get normalised cumulative progress
-cumulative_normalised = []
-for i in range(len(data_relevant)):
-    temp = np.array(
-        ast.literal_eval(data_relevant['cumulative progress'][i]))
-    cumulative_normalised.append(temp/data_relevant['Total credits'][i])
-data_relevant['cumulative progress normalised'] = cumulative_normalised
+# drop nan entries
+data_relevant = data_relevant.dropna(subset=['delta progress'])
+data_relevant = data_relevant.reset_index(drop=True)
 
-# transform delta progress in weeks from days
+# how many dropped in total
+print(f'Total subjects dropped = {len(data)-len(data_relevant)}')
+
+# get normalised cumulative progress
+data_relevant['cumulative progress normalised'] = data_relevant.apply(
+    normalize_cumulative_progress, axis=1)
+
+# transform delta progress to weeks from days
 semester_length = len(ast.literal_eval(data_relevant['delta progress'][0]))
 semester_length_weeks = round(semester_length/7)
-delta_progress_weeks = []
-for i in range(len(data_relevant)):
-
-    temp = ast.literal_eval(data_relevant['delta progress'][i])
-    temp_week = []
-    for i_week in range(semester_length_weeks):
-
-        temp_week.append(
-            sum(temp[i_week*7: (i_week+1)*7]) * 1.0)
-
-    assert sum(temp_week) == data_relevant['Total credits'][i]
-    delta_progress_weeks.append(temp_week)
-
-data_relevant['delta_progress_weeks'] = delta_progress_weeks
+data_relevant['delta_progress_weeks'] = data_relevant.apply(
+    lambda row: process_delta_progress(row, semester_length_weeks), axis=1)
 
 # get cumulative progress from delta progress
-cumulative_progress_weeks = []
-for i in range(len(data_relevant)):
-
-    cumulative_progress_weeks.append(
-        list(np.cumsum(
-            data_relevant['delta_progress_weeks'][i]))
-    )
-data_relevant['cumulative_progress_weeks'] = cumulative_progress_weeks
+data_relevant["cumulative_progress_weeks"] = data_relevant.apply(
+    cumulative_progress_weeks, axis=1)
 
 timeseries_to_cluster = np.vstack(
     data_relevant['cumulative progress normalised'])
+
+# %%
 # find and plot inertia vs cluster number
 inertia = []
 for cluster_size in range(1, 15):
-    print(f'cluster no. = {cluster_size+1}')
+
     km = KMeans(n_clusters=cluster_size+1, n_init=3,
                 random_state=0)
     labels = km.fit_predict(timeseries_to_cluster)
@@ -84,29 +96,25 @@ plt.xlabel('cluster number')
 plt.ylabel('k-means \n sum of squares')
 
 # pick best cluster number (elbow method) and cluster again
-km = KMeans(n_clusters=8, verbose=True, random_state=1, n_init=5)
+km = KMeans(n_clusters=8, verbose=False, random_state=1, n_init=5)
 labels = km.fit_predict(timeseries_to_cluster)
 data_relevant['labels'] = labels
 
+# save clustered data
+data_relevant.to_csv('data_relevant_clustered.csv', index=False)
+
 # %%
-# load clustered data
-
-data_relevant = pd.read_csv('data_relevant_clustered.csv')
-
 # plot clustered data
 
-for label in set(data_relevant['labels']):
+grouped = data_relevant.groupby('labels')
+
+for label, group in grouped:
     plt.figure(figsize=(4, 4), dpi=300)
-
-    for i in range(len(data_relevant)):
-
-        if data_relevant['labels'][i] == label:
-            # ast.literal_eval(data_relevant['delta progress'][i])
-            # data_relevant['cumulative progress normalised'][i]
-            plt.plot(
-                np.array(ast.literal_eval(
-                    data_relevant['cumulative_progress_weeks'][i])) * 2,
-                alpha=0.5)
+    for index, row in group.iterrows():
+        cumulative_progress = np.array(
+            row["cumulative_progress_weeks"]) * 2
+        plt.plot(cumulative_progress, alpha=0.5)
+    plt.title(f'Label: {label}')
     sns.despine()
     plt.xlabel('time (weeks)')
     plt.xticks([0, 7, 15])
